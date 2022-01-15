f19778d196fbc1012748fd6f490b62d148d217cb
lyrahgames <lyrahgames@mailbox.org>
2022-01-15 19:45:13 +0100

    Tweak quad-edge data structure code

--- listings/quad-edge-algebra.cpp
+++ listings/quad-edge-algebra.cpp
@@ -1,13 +1,12 @@
-struct quad_edge_algebra {
-	struct edge {
-    size_t next;
-    size_t data;
-  };
+    struct edge {
+      size_t next;
+      size_t data;
+    };
 
-  struct quad_edge {
-    edge data[4];
-  };
+    struct quad_edge {
+      edge data[4];
+    };
 
-  vector<quad_edge> quad_edges{};
-  vector<size_t> free_edges{};
-};
+    vector<vertex>    vertices{};
+    vector<quad_edge> quad_edges{};
+    vector<size_t>    free_edges{};
